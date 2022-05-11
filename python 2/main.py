c076367a533cb590df6e2d51764b7d723c66a36d
azamjon947 <105347025+azamjon947@users.noreply.github.com>
2022-05-11 15:32:43 +0500

    Add files via upload

---
+++ python 2/main.py
@@ -0,0 +1,10 @@
+name = input('Ismingizni kiriting: \n>>> ')
+
+print('Salom ' + name)
+
+age = int(input('Yoshingiz nechida?: \n>>> '))
+
+if age < 18:
+    print('Siz hali kichkina ekansz;)')
+else:
+    print('Siz ancha katta bo\'lib qolibsz')
